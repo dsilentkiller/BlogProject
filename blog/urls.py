da44142c7abfc4110fbebc2c8638727d96c2c607
dsilentkiller <paarurawal55@gmail.com>
2024-05-29 13:47:21 +0545

    resolve  url issue

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
 from blog import views
 from django.urls import path
+
+app_name = 'blog'
 urlpatterns = [
     
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
-    path('blog/post',views.PostListView.as_view(),name="post_list"),
+    path('post/',views.PostListView.as_view(),name="post_list"),
     path('post/detail/<int:pk>',views.PostDetailView.as_view(),name='post_detail'),
     path('post/delete/<int:pk>',views.PostDeleteView.as_view(),name='post_delete'),
     path('post/create',views.PostCreateView.as_view(),name='post_create'),
